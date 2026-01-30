0f961de989580722e47573ea34bbf7f5e522a396
Ratewio <ratewio@mail.ru>
2026-01-30 19:27:32 +0500

    rebuild topology

---
+++ Homework2/src/ratewio/homework2/Main.java
@@ -0,0 +1,9 @@
+package ratewio.homework2;
+
+public class Main {
+    public static void main(String[] args) {
+        int income = 50_000;
+        int spending = 37_500;
+        System.out.println("Total:\n" + (income - spending));
+    }
+}
